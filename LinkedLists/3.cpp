715732f8c8ba6471caa13e3e637a2331b1d2617b
Pedro Boueke <phboueke@gmail.com>
2019-07-07 16:50:12 -0300

    Ch 3 ex 3 done

--- LinkedLists/3.cpp
+++ LinkedLists/3.cpp
@@ -24,10 +24,9 @@ struct LinkedList{
 
     LinkedList(); //Default constructor
     ~LinkedList(); //Default destructor
-    void Add_Head(T index); //Adds element to list's head
     void Add_Tail(T index);
-    T Remove_Head(); //Removes first element
     bool Remove(T index); //Removes specific element, returns false if not in list
+    void Print();
 };
 
 // definitions =====================================================
@@ -46,14 +45,6 @@ LinkedList<T>::LinkedList(){
 }
 
 template <class T>
-LinkedList<T>::~LinkedList(){
-    //Empties list
-    while (size){
-        Remove_Head();
-    }
-}
-
-template <class T>
 void LinkedList<T>::Add_Tail(T index){
     //Adds a new element to end of list
     Element<T>* e = new Element<T>(index);
@@ -63,18 +54,37 @@ void LinkedList<T>::Add_Tail(T index){
     size++;
 }
 
-
 // 2 algorithm ==================================================
 
-int kthToLast(LinkedList<int>* ll, int k) {
-    Element<int>* curr = ll->first;
-    int limit = ll->size - k;
-    int counter = 0;
-    while (counter < limit) {
-        curr = curr->next;
-        counter += 1;
+template <class T>
+bool LinkedList<T>::Remove(T index){
+    //Removes specific element
+    Element<T>* prev = NULL;
+    for (Element<T>* it = first; it != NULL; it = it->next){
+        //Iterates list in search of specific key
+        //In case of identical keys, removes first iteration
+        if (it->index == index){
+            if (prev) prev->next = it->next;
+            else first = it->next;
+            if (!it->next) last = prev;
+            delete it;
+            --size;
+            return true;
+        }
+        prev = it;
     }
-    return curr->index;
+    return false;
+}
+
+template <class T>
+void LinkedList<T>::Print(){
+    // prints all indexes of the LL
+    Element<T>* curr = first;
+    for (int i = 0; i < size; i++) {
+        std::cout << curr->index << " -> ";
+        curr = curr->next;
+    }
+    std::cout <<" NULL \n ";
 }
 
 int main(){
@@ -90,8 +100,9 @@ int main(){
     ll->Add_Tail(8);
     ll->Add_Tail(9);
     ll->Add_Tail(10);
-
-    std::cout << "Kth to last is: " << kthToLast(ll, 3) << "\n";
+    ll->Print();
+    ll->Remove(5);
+    ll->Print();
 
     return 0;
 }
